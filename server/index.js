de9ef0b418aa02759711a264a3833433b066954c
Caryn Shu <bugbugshu@gmail.com>
2020-03-25 15:53:04 -0700

    delete some unused code

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/../public/dist')));
-let port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '/../client')));
+let port = 3000;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
