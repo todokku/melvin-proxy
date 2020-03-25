8ef71573ccc47d86f7df955324e91e3eac9bb2db
Caryn Shu <bugbugshu@gmail.com>
2020-03-24 17:05:00 -0700

    proxy running but no styling

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public/dist')));
 let port = process.env.PORT || 3000;
 app.listen(port, function() {
